cd69b071cbdcb007b068581b808ceb410d5c0f35
nurjigit1709-png <nurjigit1709@gmail.com>
2026-05-31 20:01:08 +0600

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 import path from "path";
 
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
 export default defineConfig({
-  schema: path.join(__dirname, "./src/schema/index.ts"),
+  schema: path.join(process.cwd(), "lib/db/src/schema/index.ts"),
+  out: path.join(process.cwd(), "lib/db/drizzle"),
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL || "",
   },
 });
