49d9da59e0a1dd968afcf95e0efdeae33d57faf1
nurjigit1709-png <nurjigit1709@gmail.com>
2026-05-31 20:05:21 +0600

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "drizzle-kit";
 import path from "path";
 
 export default defineConfig({
-  schema: path.join(process.cwd(), "lib/db/src/schema/index.ts"),
-  out: path.join(process.cwd(), "lib/db/drizzle"),
+  schema: path.join(__dirname, "src/schema/index.ts"),
+  out: path.join(__dirname, "drizzle"),
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL || "",
